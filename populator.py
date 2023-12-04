5a7d33d664634c59e5bc587267a6288cc967823d
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2023-12-04 17:09:27 +0300

    add populator file for mysql dbs-populate from excel files

--- populator.py
+++ populator.py
@@ -1,20 +1,24 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os 
+from dotenv import load_dotenv
+from sqlalchemy import text
+
+load_dotenv() 
 
 # Replace the following with your MySQL server details
-username = 'your_username'
-password = 'your_password'
-host = 'localhost'  # or your server IP
+username = os.getenv("db_user")
+password = os.getenv("password")
+host = os.getenv("db_host")
 database_name = 'metakocka_db'
 
 # Creating the database connection
-engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}/')
+engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}/{database_name}')
 
 # Create a new database (if it doesn't exist)
 with engine.connect() as conn:
-    conn.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
-    conn.execute(f"USE {database_name}")
-
+    conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {database_name}"))
+    conn.execute(text(f"USE {database_name}"))
 # Read data from Excel file
 df = pd.read_excel('Metakocka.xlsx', engine='openpyxl')
 
